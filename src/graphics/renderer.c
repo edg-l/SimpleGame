613756502b010cd533f2101716e1aaa17be763da
Ryozuki <ryo@ryozuki.xyz>
2019-08-18 15:24:38 +0200

    make rect us ebo

--- src/graphics/renderer.c
+++ src/graphics/renderer.c
@@ -178,26 +178,34 @@ int render_init(int width, int height, const char *title) {
 
 	// Setup the quad VAO
 	GLuint vbo;
+	GLuint ebo;
 
 	GLfloat vertices[] = {
-		0, 1,
-		1, 0,
-		0, 0,
+		0, 1, // top left
+		1, 1, // top right
+		1, 0, // bottom right
+		0, 0, // bottom left
+	};
 
-		0, 1,
-		1, 1,
-		1, 0
+	GLuint indices[] = {
+		0, 1, 2,
+		2, 3, 0
 	};
 
 	glGenVertexArrays(1, &quadVAO);
 	glGenBuffers(1, &vbo);
+	glGenBuffers(1, &ebo);
+
+	glBindVertexArray(quadVAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	glBindVertexArray(quadVAO);
-	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
 
 	log_write(LOG_INFO, "Renderer initialized.\n");
 
@@ -247,8 +255,11 @@ void render_rect(float x, float y, float width, float height, int filled) {
 	shader_set_mat4(quadShader, "model", model);
 
 	glBindVertexArray(quadVAO);
-	//glDrawArrays(GL_TRIANGLES, 0, 6);
-	glDrawArrays(GL_LINE_STRIP, 0, 6);
+	
+	if(filled)
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+	else
+		glDrawElements(GL_LINE_STRIP, 6, GL_UNSIGNED_INT, 0);
 	glBindVertexArray(0);
 }
 
